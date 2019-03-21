333b4b5ab571f5710189f331414c43664c0b5a0f
Jinhongli <jhl211@163.com>
2019-03-21 20:41:53 +0800

    :sparkles: add spy core functionality.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,64 @@
 let IS_SPY_ON = false;
 let LOGS = [];
 
-const realXHR = window.XMLHttpRequest;
+let _open;
+let _send;
+
+function insertLog(log) {
+  console.log(log);
+  LOGS.push(log);
+}
 
 function startSpy() {
-  if (!realXHR) {
-    console.warn('XMLHttpRequest is not supported!');
-    return false;
-  }
-  const realOpen = window.XMLHttpRequest.prototype.open;
-  const realSend = window.XMLHttpRequest.prototype.send;
+  if (!window.XMLHttpRequest || IS_SPY_ON) return false;
+  IS_SPY_ON = true;
+  _open = window.XMLHttpRequest.prototype.open;
+  _send = window.XMLHttpRequest.prototype.send;
 
   window.XMLHttpRequest.prototype.open = function(...args) {
     const xhr = this;
-    console.log(xhr);
-    return realOpen.apply(xhr, args);
+    xhr.log = {
+      method: args[0],
+      url: args[1],
+    };
+    return _open.apply(xhr, args);
   };
 
   window.XMLHttpRequest.prototype.send = function(...args) {
     const xhr = this;
-    console.log(xhr);
-    return realSend.apply(xhr, args);
+    const sendTime = Date.now();
+    const _onreadystatechange = xhr.onreadystatechange;
+    xhr.onreadystatechange = function() {
+      switch (xhr.readyState) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+          break;
+        case 4:
+          Object.assign(xhr.log, {
+            status: xhr.status,
+            statusText: xhr.statusText,
+            time: Date.now() - sendTime,
+            data: xhr.response,
+          });
+          insertLog(xhr.log);
+        default:
+          break;
+      }
+      return _onreadystatechange.apply(xhr, args);
+    };
+    return _send.apply(xhr, args);
   };
 }
 
-export { startSpy };
+function stopSpy() {
+  window.XMLHttpRequest.prototype.open = _open;
+  window.XMLHttpRequest.prototype.send = _send;
+}
+
+function getLogs() {
+  return LOGS;
+}
+
+export { startSpy, stopSpy, getLogs };
